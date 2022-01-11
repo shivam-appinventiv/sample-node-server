a5e6d4640e5fb924a969a544bbe1d25c94e02667
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2022-01-11 13:32:06 +0530

    run script updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-cd /home/ec2-user/node
-npm install
-sudo pm2 delete prod
-sudo pm2 delete pm2-logrotate
-pm2 start server.js --name prod
-pm2 reload prod
+cd /home/ec2-user/stg
+pm2 restart stg
