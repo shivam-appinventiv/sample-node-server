0092596dcc1c740349161cc81ca814bfed2ea15c
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2022-01-11 16:56:00 +0530

    comment added on prod branch

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 8080 ;
 
 console.log('PORT',PORT);
 
-app.get('/',(req,res)=> res.send(`Hello world from node server with CI/CD V1 ${req.socket.remoteAddress} on port : ${PORT}`));
+app.get('/',(req,res)=> res.send(`Hello world from node server with CI/CD V1 from prod ${req.socket.remoteAddress} on port : ${PORT}`));
 
-
+// comment added on prod branch
 app.listen(PORT,()=>{
   console.log(`Example app listening at http://localhost:${PORT}`);
 })
